b5738f36e28ecf4dd3af235297f5dbffb659f061
Saxon Hunt <shunt@twilio.com>
2022-09-29 22:51:58 -0700

    Updates

--- pagefind_script.js
+++ pagefind_script.js
@@ -5,27 +5,12 @@
 // 	});
 // }
 
-var observer = new MutationObserver(function (mutations) {
-	if (document.getElementById('search').length) {
-		// console.log('Exist, lets do something');
+function check_for_search_and_wait() {
+	if (!document.getElementById('search')) {
+		setTimeout(check_for_search_and_wait, 1000);
+	} else {
 		new PagefindUI({ element: '#search' });
-		observer.disconnect();
-		//We can disconnect observer once the element exist if we dont want observe more changes in the DOM
 	}
-});
+}
 
-// Start observing
-observer.observe(document.body, {
-	//document.body is node target to observe
-	childList: true, //This is a must have for the observer with subtree
-	subtree: true //Set to true if changes must also be observed in descendants.
-});
-
-// $(document).ready(function () {
-// 	$('button').on('click', function () {
-// 		$('p').remove();
-// 		setTimeout(function () {
-// 			$('#newContent').append('<p>New element</p>');
-// 		}, 2000);
-// 	});
-// });
+check_for_search_and_wait();
