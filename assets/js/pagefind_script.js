c69ef448d43381b5eb91733add655d9472b96daf
Saxon Hunt <shunt@twilio.com>
2022-09-29 22:32:49 -0700

    Updates

--- assets/js/pagefind_script.js
+++ assets/js/pagefind_script.js
@@ -1,6 +1,8 @@
-if (typeof window !== 'undefined') {
-	// browser code
-	window.addEventListener('DOMContentLoaded', (event) => {
-		new PagefindUI({ element: '#search' });
-	});
-}
+// if (typeof window !== 'undefined') {
+// 	// browser code
+// 	window.addEventListener('DOMContentLoaded', (event) => {
+// 		new PagefindUI({ element: '#search' });
+// 	});
+// }
+
+new PagefindUI({ element: '#search' });
